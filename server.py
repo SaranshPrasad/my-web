e16228c6a900f23964cade5ce406bd915ae4c67b
SaranshPrasad <131943503+SaranshPrasad@users.noreply.github.com>
2024-01-20 21:32:18 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,15 +1,8 @@
 from flask import Flask, render_template, request, send_from_directory, url_for
-import smtplib
-from dotenv import load_dotenv
-import os
 
 
-load_dotenv()
-
-password = os.environ.get('PASSWORD')
 app = Flask(__name__)
-my_email = os.environ.get("my_email")
-PORT = 587
+
 
 
 @app.route('/')
