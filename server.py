ad695021faf4fc5bc40c3adede8dcf4a64cbc4ba
SaranshPrasad <131943503+SaranshPrasad@users.noreply.github.com>
2024-01-20 21:31:58 +0530

    Update server.py

--- server.py
+++ server.py
@@ -19,17 +19,5 @@ def home():
 def download():
     return send_from_directory('static', path="files/my_cv.docx")
 
-@app.route('/contact', methods=["GET", "POST"])
-def contact():
-    if request.method == "POST":
-        name = request.form['name']
-        email = request.form['email']
-        mess = request.form['message']
-        with smtplib.SMTP("smtp.gmail.com", port=PORT) as conn:
-            conn.starttls()
-            conn.login(user=my_email, password=password)
-            conn.sendmail(from_addr=my_email, to_addrs=my_email,
-                          msg=f"Subject:{name} Contacted You!\n\nName: {name}\nEmail: {email}\nMessage: {mess}")
-            return render_template('index.html')
-    return render_template('contact.html')
+
 
